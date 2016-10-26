d928f5612d9f4ce7ace4cc406e6b05590d7eaa41
Kyle Rasch <kyle.rasch@pcc.edu>
2016-10-25 21:11:23 -0700

    added name in comments

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -2,7 +2,7 @@ package edu.pcc.cis.helloworld;
 import java.util.Date;
 
 /**
- * Created by kyle on 10/25/16.
+ * Created by kyle Rasch on 10/25/16.
  */
 public class HelloWorld {
     public static void main(String[] args) {

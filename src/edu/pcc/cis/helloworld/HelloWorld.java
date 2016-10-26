3bd10de0252ed02f43bb33cab5843374035a2d1f
Kyle Rasch <kyle.rasch@pcc.edu>
2016-10-25 21:03:58 -0700

    added main method to print hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,4 +4,7 @@ package edu.pcc.cis.helloworld;
  * Created by kyle on 10/25/16.
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
